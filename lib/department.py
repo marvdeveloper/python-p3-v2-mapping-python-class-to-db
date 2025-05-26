15f7718ed1bc3af73a9cf32e6d1f7e6c2bd2bdc2
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-26 18:51:54 +0300

    Add complete CRUD methods to Department class

--- lib/department.py
+++ lib/department.py
@@ -1,12 +1,68 @@
-from __init__ import CURSOR, CONN
+# department.py
 
+import sqlite3
+
+CONN = sqlite3.connect('company.db')
+CURSOR = CONN.cursor()
 
 class Department:
-
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
         self.location = location
 
-    def __repr__(self):
-        return f"<Department {self.id}: {self.name}, {self.location}>"
+    @classmethod
+    def create_table(cls):
+        """Create the departments table if it doesn't exist."""
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """Drop the departments table if it exists."""
+        sql = "DROP TABLE IF EXISTS departments"
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        """Insert a new row into departments and assign the id to the instance."""
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, location):
+        """Creates a new Department instance and saves it to the database."""
+        department = cls(name, location)
+        department.save()
+        return department
+
+    def update(self):
+        """Update the corresponding row in the database."""
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        """Delete the corresponding row in the database."""
+        sql = """
+            DELETE FROM departments
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
